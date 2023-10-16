fccc47d1a14569ef40357b7b013b961213a1625a
emkodelirdi <73055059+emkodelirdi@users.noreply.github.com>
2023-10-16 12:21:52 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
-curl -o node.pkg  https://nodejs.org/dist/v18.18.2/node-v18.18.2.pkg
-sudo installer -pkg /node.pkg -target /
+#!/bin/bash
+
+# curl -o node.pkg  https://nodejs.org/dist/v18.18.2/node-v18.18.2.pkg
+if type node > /dev/null 2>&1 && which node > /dev/null 2>&1 ;then
+    node -v
+    echo "node is installed, skipping..."
+else
+    echo "installing node"
+    curl -o node.pkg  https://nodejs.org/dist/v18.18.2/node-v18.18.2.pkg
+    sudo installer -pkg /node.pkg -target /
+fi
+# sudo installer -pkg /node.pkg -target /
 npm install electron
 npm install electron-builder
 npm run dist
